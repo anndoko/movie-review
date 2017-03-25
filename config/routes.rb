3d5abeac55ba7f5aa4f5bd2c919f277fe38e84cb
anndoko <handcuffanndo@gmail.com>
2017-03-25 13:27:45 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
 
     resources :posts
   end
+
+  namespace :account do
+    resources :movies
+  end
+
+
+
     root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
