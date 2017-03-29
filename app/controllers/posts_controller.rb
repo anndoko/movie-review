9529af6a016df3f85273d7b1d75cf4a25b3255d8
anndoko <handcuffanndo@gmail.com>
2017-03-30 01:10:55 +0800

    user can only comment on their favorite movies

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ before_action :authenticate_user!, :only => [:new, :create]
 def new
   @movie = Movie.find(params[:movie_id])
   @post = Post.new
+
+  if !current_user.is_member_of?(@movie)
+    redirect_to movie_path(@movie), alert: "This movie is not in your favorite."
+  end
 end
 
 def create
